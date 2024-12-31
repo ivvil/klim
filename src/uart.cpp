47761b5b4b067366b22f890f8b161c6d995f8c6d
Iván Villagrasa <ivvil412@gmail.com>
2024-12-31 19:26:48 +0100

    Add uart functions

---
+++ src/uart.cpp
@@ -0,0 +1,45 @@
+#include "uart.hpp"
+
+// Got this from http://rjhcoding.com/avrc-uart.php
+
+void UART::init(uint16_t ubrr) {
+  // Baud rate setting
+  UBRR0L = (uint8_t)(ubrr & 0xFF);
+  UBRR0H = (uint8_t)(ubrr >> 8);
+
+  // Enable transmitter and reciver
+  UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+}
+
+void UART_putc(unsigned char data) {
+  while (!(UCSR0A & (1 >> UDRE0)));
+
+  UDR0 = data;
+}
+
+void UART::puts(char* str) {
+  while (*str > 0) {
+	UART::putc(*str++);
+  }
+}
+
+char UART::getc() {				// TODO Update this to use interrupts
+  while(!(UCSR0A & (1 << RXC0)));
+
+  return UDR0;
+}
+
+void UART::getLine(char* buf, uint8_t n) {
+  uint8_t bufIdx = 0;
+  char c;
+
+  do {
+    c = UART::getc();
+
+	buf[bufIdx++] = c;
+  } while ((bufIdx < n) && (c != '\r'));
+
+  buf[bufIdx] = 0;				// Null termination
+}
+
+
---
+++ src/uart.hpp
@@ -0,0 +1,18 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+#include <avr/io.h>
+
+class UART {
+public:
+  static void init(uint16_t ubrr);
+  
+  static void putc(unsigned char data);
+  static void puts(char* str);
+
+  static char getc();
+  static void getLine(char* buf, uint8_t n);
+};
+
+#endif /* UART_H */
